bef52a40ccbda37780fb8d74a0dfdd6a28e55841
Ravanpreet Kaur Bagga <73405757+Preet1406@users.noreply.github.com>
2022-04-04 10:10:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def signup():
         new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        return render_template('login.html', message = "New user has been created!")
+        return render_template('login.html', form=Login_form, message = "New user has been created!")
 
     return render_template('signup.html', form=form)
 
